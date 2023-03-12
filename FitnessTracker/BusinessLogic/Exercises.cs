1754b9f368291f21befc2a6af84df05e4940c870
Kunal Bajaj <bajajku@sheridancollege.ca>
2023-03-12 16:03:34 -0400

    created muscle enum and added cardio class inheriting from exercises class in Exercises module.

--- FitnessTracker/BusinessLogic/Exercises.cs
+++ FitnessTracker/BusinessLogic/Exercises.cs
@@ -7,11 +7,30 @@ using System.Threading.Tasks;
 namespace FitnessTracker.BusinessLogic
 {
     // class to store exercises
-    class Exercises
-    {
+   public class Exercises
+   {
         string _name;
         string _description;
-        DifficultyLevel difficultyLevel;
+        DifficultyLevel _difficultyLevel;
+        MuscleGroup _targetMuscle;
+        
+        public Exercises(string name, string description, DifficultyLevel difficultyLevel, MuscleGroup targetMuscle)
+        {
+            _name = name;
+            _description = description;
+            _difficultyLevel = difficultyLevel;
+            _targetMuscle= targetMuscle;
+        }
 
+   }
+    public class Cardio : Exercises
+    {
+        float _calorieBurned;
+
+        public Cardio(float calorieBurned,string name, string description, DifficultyLevel difficultyLevel, MuscleGroup targetMuscle): base(name,description,difficultyLevel,targetMuscle)
+        {
+            _calorieBurned = calorieBurned;
+        }
     }
+
 }
